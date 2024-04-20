19ea179a36063c33dcc53f365e94a6e1cc5598bf
nazhus <143502377+nazhus@users.noreply.github.com>
2024-04-20 22:16:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const countDivs = document.querySelectorAll('.count');
+const targets = Array.from(document.querySelectorAll('.counter-item')).map(item => parseInt(item.getAttribute('data-target')));
+
+let counts = Array.from({ length: countDivs.length }, () => 0);
+const incrementDelay = 10; // Adjust the delay (in milliseconds) as needed
+
+function startIncrement() {
+  for (let i = 0; i < countDivs.length; i++) {
+    setInterval(() => {
+      if (counts[i] < targets[i]) {
+        counts[i]++;
+        countDivs[i].textContent = counts[i];
+      }
+    });
+  }
+}
+
+// Start the incrementing process
+startIncrement();
