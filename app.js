afdd597e0106e393dc597948a7c83db9c3fe0cc6
nazhus <143502377+nazhus@users.noreply.github.com>
2024-04-24 11:23:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const countDivs = document.querySelectorAll('.count');
 const targets = Array.from(document.querySelectorAll('.counter-item')).map(item => parseInt(item.getAttribute('data-target')));
 
 let counts = Array.from({ length: countDivs.length }, () => 0);
-const incrementDelay = 10; // Adjust the delay (in milliseconds) as needed
-
+const incrementDelay = 10; 
 function startIncrement() {
   for (let i = 0; i < countDivs.length; i++) {
     setInterval(() => {
@@ -15,5 +14,5 @@ function startIncrement() {
   }
 }
 
-// Start the incrementing process
+
 startIncrement();
